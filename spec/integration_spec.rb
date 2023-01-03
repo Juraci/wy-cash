1be4f8eeb2f9a3e12081b0117771ae3ffda045e8
Juraci <juraci.vieira@gmail.com>
2023-01-03 14:48:59 -0300

    shameless green Franc

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,9 +1,17 @@
 require "spec_helper"
 require "dollar"
+require "franc"
 
 RSpec.describe "Integration specs" do
   it "multiplies a given amount" do
     five = Dollar.new(amount: 5)
+
+    expect(five.times(2)).to be_equals(Dollar.new(amount: 10))
+    expect(five.times(3)).to be_equals(Dollar.new(amount: 15))
+  end
+
+  it "multiplies a given amount" do
+    five = Franc.new(amount: 5)
 
     expect(five.times(2)).to be_equals(Dollar.new(amount: 10))
     expect(five.times(3)).to be_equals(Dollar.new(amount: 15))
